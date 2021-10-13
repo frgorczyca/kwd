1712416b5ec57b5ec11689ebfb735dc7cc9fb77e
Franciszek Gorczyca <franciszekgorczyca@sztarbuks.local>
2021-10-13 21:52:46 +0200

    add database connection setup

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const sqlite = require('sqlite3').verbose();
 
 let app = express();
+let db = new sqlite.Database("../database/office_status_db", (err) => {
+    if(err) {
+        console.error(err.message);
+    }
+
+    console.log("Connected to database");
+})
 
 app.use(express.json());
 
 app.get("/biuro_status", function (req, res) {
-    res.send("This is an automated reposne");
+    db.each("SELECT * FROM OfficeStatus limit 1", (err, row) => {
+        res.send(row)
+    })
 })
 
 app.get('/', function (req, res) {
-    res.send("Default response")
+    res.send("Default response");
 })
 
 app.listen(5000);
