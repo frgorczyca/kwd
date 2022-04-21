2a2721a4bc3fa237b3124cf63ecfa944370897bd
Izdnozrkenar <fr.gorczyca@gmail.com>
2022-04-21 23:06:20 +0200

    feat: Add interrupt handling to read limit switch from gpio

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const fs = require("fs");
+const gpio = require("onoff").Gpio;
+
+let nc = new gpio(17, 'in', "both", { debounceTimeout: 10});
+let no = new gpio(27, 'in');
 
 let app = express();
+
+const errorValue = "Error";
+const occupiedValue = "Occupied";
+const emptyValue = "Empty";
+
+const statusFilePath = "office_status.txt";
+
+nc.watch(function(err, value) {
+    if(err) {
+	fs.writeFileSync(statusFilePath, errorValue)
+    } else if (value == 1) {
+	fs.writeFileSync(statusFilePath, occupiedValue)
+    } else {
+	fs.writeFileSync(statusFilePath, emptyValue)
+    }
+})
 
 app.use(express.json());
 
 app.get("/office_status", function (req, res) {
-    fs.readFile("office_status.txt", "utf8", (err, data) => {
+    fs.readFile(statusFilePath, "utf8", (err, data) => {
         if (err) {
-            res.send(err)
+            res.send(err);
         }
-        res.send(data)
+        res.send(data);
     })
 })
 
 app.get('/', function (req, res) {
+    console.log(nc.readSync(), no.readSync());
     res.send("Default response");
 })
 
-app.listen(3000);
+app.listen(80);
